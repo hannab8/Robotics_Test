ca2db6f1ab6ed68bd26eb977f1832105c51a0062
hannab8 <83167499+hannab8@users.noreply.github.com>
2023-11-17 18:07:09 -0500

    Update stereo.py

--- stereo.py
+++ stereo.py
@@ -1,36 +1,3 @@
-import cv2
-import numpy as np
-img1 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/left_image.jpg')
-img2 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/right_image.jpg')
-
-imgs = [img1,img2]
-print("img1 size:",img1.shape)
-print("img2 size:",img2.shape)
-
-cv2.imshow("left",img1)
-cv2.imshow("right",img2)
-
-print("Creating stitcher...")
-stitcher=cv2.Stitcher.create() 
-(status,output)=stitcher.stitch(imgs) 
-
-print("Checking stitcher...")
-if status != cv2.STITCHER_OK: 
-  # checking if the stitching procedure is successful 
-  # .stitch() function returns a true value if stitching is  
-  # done successfully 
-    print("stitching ain't successful") 
-else:  
-    print('Your Panorama is ready!!!') 
-    
-  
-# final output 
-cv2.imshow('final result',output) 
-  
-cv2.waitKey(0)
-
-
-
 #!/usr/bin/env python3
 
 import rospy
@@ -60,114 +27,30 @@ def callback(config, level):
 def left_img_cb(msg):
     global left_image
     left_image = br.imgmsg_to_cv2(msg)
+    # cv2.imshow("left image", left_image)
+    # cv2.waitKey(1)
+    if left_image is not None and right_image is not None:
+        stitch_images()
+        # print("Both filled")
+        # cv2.imshow("right image", img)
+        # cv2.waitKey(1)
 
 def right_img_cb(msg):
     global right_image
     right_image = br.imgmsg_to_cv2(msg)
+    # cv2.imshow ("left",left_image)
+    # cv2.waitKey(1)
 
-def process_image(img):
-    blur = cv2.GaussianBlur(img, (3, 3), 0)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
-    lower_white = np.array([h_low, s_low, v_low], np.uint8)
-    upper_white = np.array([h_high, s_high, v_high], np.uint8)
-
-    mask = cv2.inRange(hsv, lower_white, upper_white)
-
-    h, w = mask.shape[:2]
-
-    # Clear top fraction of the image
-    f = 0.2
-    mask[0:int(np.ceil(f * h)), 0:w] = 0
-
-    # Set mask on robot body
-    xs, xe = int(np.ceil(0.3 * w)), int(np.ceil(0.7 * w))
-    ys = int(np.ceil(0.5 * h))
-    mask[ys:h, xs:xe] = 0
-
-    # Hough lines to remove spray
-    mask_lanes = mask.copy()
-    mask_lanes[0:h, 0:w] = 0
-
-    hough = cv2.HoughLinesP(image=mask, rho=1, theta=10 * (np.pi) / 180, threshold=50, minLineLength=50, maxLineGap=100)
-    if hough is not None:
-        for points in hough:
-            x0, y0, x1, y1 = points[0]
-            cv2.line(mask_lanes, (x0, y0), (x1, y1), color=(255, 255, 255), thickness=2)
-
-    stacked_img = np.stack((mask_lanes,) * 3, axis=-1)
-    return stacked_img
-
-def stitch_images(left_img, right_img):
-    # Use OpenCV's stitcher to combine images
-    stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
-    (status, stitched) = stitcher.stitch([left_img, right_img])
-    
-    if status == cv2.Stitcher_OK:
-        return stitched
-    else:
-        rospy.loginfo("Image stitching failed.")
-        return None
-
-def publish_panoramic():
+def img_cb(msg): 
     global left_image, right_image
-
+    
     if left_image is not None and right_image is not None:
-        left_processed = process_image(left_image)
-        right_processed = process_image(right_image)
+        img = stitch_images()
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
+    #img = br.imgmsg_to_cv2(msg)
 
-        panoramic_img = stitch_images(left_processed, right_processed)
-        if panoramic_img is not None:
-            pub.publish(br.cv2_to_imgmsg(panoramic_img, "bgr8"))
-
-if __name__ == "__main__":
-    rospy.init_node("hsv_thresh", anonymous=False)
-    pub = rospy.Publisher("/panoramic_lane_img", Image, queue_size=10)
-    cfg = Server(HSVConfig, callback)
-    rospy.Subscriber("/zed2/zed_node/left/image_rect_color", Image, left_img_cb)
-    rospy.Subscriber("/zed2/zed_node/right/image_rect_color", Image, right_img_cb)
-
-    rate = rospy.Rate(10)  # Adjust the rate as needed
-    while not rospy.is_shutdown():
-        publish_panoramic()
-        rate.sleep()
-
-
-
-
-
-
-
-
-#!/usr/bin/env python3
-
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-import cv2
-import numpy as np
-from dynamic_reconfigure.server import Server
-from hsv_thresh.cfg import HSVConfig
-
-br = CvBridge()
-
-# Initialize HSV mask parameters
-h_low, s_low, v_low = 0, 0, 177
-h_high, s_high, v_high = 124, 15, 255
-
-
-def callback(config, level):
-    global h_low, s_low, v_low, h_high, s_high, v_high
-    h_low, s_low, v_low = config.H_low, config.S_low, config.V_low
-    h_high, s_high, v_high = config.H_high, config.S_high, config.V_high
-    return config
-
-
-def img_cb(msg):
-
-    # hsv_channels = []
-    img = br.imgmsg_to_cv2(msg)
-    # print(img.shape)
+    return
     blur = cv2.GaussianBlur(img, (3, 3), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
     # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -176,67 +59,6 @@ def img_cb(msg):
     upper_white = np.array([h_high, s_high, v_high], np.uint8)
 
     mask = cv2.inRange(hsv, lower_white, upper_white)
-
-
-# ------------------------------------------------------------------------------------------------------------------
-    # Split the stereo image into left and right images
-    # Hanna's attempt to merge Left and Right Image into Panorama
-
-    # height, width, _ = img.shape
-    # left_image = img[:, :width // 2, :]
-    # right_image = img[:, width // 2:, :]
-
-    # stitcher = cv2.createStitcher()  # Create a stitcher object
-    # result, img = stitcher.stitch([left_image, right_image])
-
-    # pan_pub.publish(br.cv2_to_imgmsg(img, "bgr8"))
-
-# ------------------------------------------------------------------------------------------------------------------
-
-    # window = np.zeros(img.shape[:2], dtype="uint8")
-
-    # #Extract rectangles
-    # er = 3
-    # kernel_rect = cv2.getStructuringElement(cv2.MORPH_RECT, (er,er))
-    # rect_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel_rect, iterations=2)
-
-    # #Remove sprays
-    # kernel = np.ones((3,3),np.uint8)
-    # mask_lanes = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
-    # #Find Hough circles
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.medianBlur(gray, 5)
-    # rows = gray.shape[0]
-    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 64,
-    #                            param1=200, param2=20,
-    #                            minRadius=20, maxRadius=30)
-
-    # # print(circles)
-    # # print(mask.shape)
-
-    # #Create a mask of squares of same radius as that of detected circles
-    # square_mask = 255-0*mask_open.copy()
-    # #Draw circles
-    # if circles is not None:
-    #     circles = np.uint16(np.around(circles))
-    #     for i in circles[0, :]:
-    #         center = (i[0], i[1])
-    #         #Start defining mask
-    #         square_mask[i[1]-2*i[2]:i[1]+2*i[2],i[0]-2*i[2]:i[0]+2*i[2]] = 0
-    #         # circle center
-    #         cv2.circle(img, center, 1, (0, 100, 100), 3)
-    #         # circle outline
-    #         radius = i[2]
-    #         cv2.circle(img, center, radius, (255, 0, 255), 3)
-
-    # cv2.imshow("grayscale image",gray)
-    # cv2.imshow("detected circles", img)
-    # cv2.imshow("square mask",square_mask)
-    # # print(square_mask)
-
-    # #Bitwise_AND the square mask and lanes extracted
-    # mask_lanes = cv2.bitwise_and(mask_open,square_mask)
 
     h = len(mask)
     w = len(mask[1])
@@ -260,27 +82,10 @@ def img_cb(msg):
         for points in hough:
             x0, y0, x1, y1 = points[0]
             cv2.line(mask_lanes, (x0, y0), (x1, y1), color=(255, 255, 255), thickness=2)
-
-            # mask_lanes[y0,x0] = 255
-            # mask_lanes[y1,x1] = 255
-
-        # Hough transforms for finer lane extraction
-
+            
         # Show spray removed lane extraction
         cv2.imshow('Spray removed', mask_lanes)
 
-        # stacked_img = np.stack((mask_lanes,) * 3, axis=-1)
-
-        # # cv2.split(mask, hsv_channels)
-
-        # # Publish image to \lane_img
-        # pub.publish(br.cv2_to_imgmsg(stacked_img, "bgr8"))
-
-        # # print(mask.shape)
-        # cv2.imshow("image", img)
-        # cv2.imshow("HSVed", mask)
-
-        # cv2.waitKey(1)
     except:
         print("no lanes seen")
         mask_lanes[0:h, 0:w] = 0
@@ -294,15 +99,69 @@ def img_cb(msg):
 
     cv2.waitKey(1)
 
+def stitch_images():
+    global left_image, right_image
+    
+    img1 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/left_image.jpg')
+    img2 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/right_image.jpg')
+    imgs = [img1,img2]
+    # Use OpenCV's stitcher to combine images
+    
+    print("Stitching Images...")
+    #imgs = [left_image,right_image]
+    stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
+    (status, stitched) = stitcher.stitch(imgs)
+    
+    print("After Stitch...")
+    if status == cv2.Stitcher_OK:
+        print("STITCHED PROPERLY")
+        cv2.imshow("image", stitched)
+        cv2.waitKey(1)
+        return stitched
+    else:
+        rospy.loginfo("Image stitching failed.")
+        return None
+    
 
 if __name__ == "__main__":
 
     rospy.init_node("hsv_thresh", anonymous=False)
     pub = rospy.Publisher("/lane_img", Image, queue_size=10)
-    #pan_pub = rospy.Publisher("/pan_image", Image, queue_size=10)
     cfg = Server(HSVConfig, callback)
-    rospy.Subscriber("/zed2i/zed_node/left/image_rect_color", Image, img_cb)
-    #rospy.Subscriber("/zed2i/zed_node/stereo_raw/image_raw_color", Image, img_cb)
-    # rospy.Subscriber("/camera_2d/image_raw",Image,img_cb)
+    rospy.Subscriber("/zed2i/zed_node/left/image_rect_color", Image, left_img_cb)
+    rospy.Subscriber("/zed2i/zed_node/right/image_rect_color", Image, right_img_cb)
     rospy.spin()
 
+# THIS HAS PROVEN TO WORK
+
+import cv2
+import numpy as np
+img1 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/left_image.jpg')
+img2 = cv2.imread('/home/aasha/karina_ws/src/Karina/igvc_perception/hsv_thresh/src/right_image.jpg')
+
+imgs = [img1,img2]
+print("img1 size:",img1.shape)
+print("img2 size:",img2.shape)
+
+cv2.imshow("left",img1)
+cv2.imshow("right",img2)
+
+print("Creating stitcher...")
+stitcher=cv2.Stitcher.create(cv2.Stitcher_SCANS)
+#stitcher.setPanoConfidenceThresh(0.0) 
+(status,output)=stitcher.stitch(imgs) 
+
+print("Checking stitcher...")
+if status != cv2.STITCHER_OK: 
+  # checking if the stitching procedure is successful 
+  # .stitch() function returns a true value if stitching is  
+  # done successfully 
+    print("stitching ain't successful") 
+else:  
+    print('Your Panorama is ready!!!') 
+    
+  
+# final output 
+cv2.imshow('final result',output) 
+  
+cv2.waitKey(0)
