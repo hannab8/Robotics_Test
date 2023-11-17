90f3f6c5f26ee4cd98a8793e76d6f4e2698661b8
hannab8 <83167499+hannab8@users.noreply.github.com>
2023-11-17 14:10:36 -0500

    ROSified Code & included Changes

--- stereo.py
+++ stereo.py
@@ -1,68 +1,100 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from dynamic_reconfigure.server import Server
+from hsv_thresh.cfg import HSVConfig
 
-def load_and_split_stereo_image(image_path):
-    # Load the stereo image
-    stereo_img = cv2.imread(image_path)
+br = CvBridge()
 
-    # Split the image into left and right
-    height, width, _ = stereo_img.shape
-    left_img = stereo_img[:, :width // 2]
-    right_img = stereo_img[:, width // 2:]
+# Initialize HSV mask parameters
+h_low, s_low, v_low = 0, 0, 177
+h_high, s_high, v_high = 124, 15, 255
 
-    return left_img, right_img
+# Global variables to store left and right images
+left_image = None
+right_image = None
 
-def detect_and_match_features(left_img, right_img):
-    # Convert images to grayscale
-    gray_left = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
-    gray_right = cv2.cvtColor(right_img, cv2.COLOR_BGR2GRAY)
+def callback(config, level):
+    global h_low, s_low, v_low, h_high, s_high, v_high
+    h_low, s_low, v_low = config.H_low, config.S_low, config.V_low
+    h_high, s_high, v_high = config.H_high, config.S_high, config.V_high
+    return config
 
-    # Initialize the ORB detector
-    orb = cv2.ORB_create()
+def left_img_cb(msg):
+    global left_image
+    left_image = br.imgmsg_to_cv2(msg)
 
-    # Detect keypoints and descriptors
-    keypoints_left, descriptors_left = orb.detectAndCompute(gray_left, None)
-    keypoints_right, descriptors_right = orb.detectAndCompute(gray_right, None)
+def right_img_cb(msg):
+    global right_image
+    right_image = br.imgmsg_to_cv2(msg)
 
-    # Match features using BFMatcher
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = matcher.match(descriptors_left, descriptors_right)
+def process_image(img):
+    blur = cv2.GaussianBlur(img, (3, 3), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    # Sort matches by distance
-    matches = sorted(matches, key=lambda x: x.distance)
+    lower_white = np.array([h_low, s_low, v_low], np.uint8)
+    upper_white = np.array([h_high, s_high, v_high], np.uint8)
 
-    # Extract matched keypoints
-    points_left = np.zeros((len(matches), 2), dtype=np.float32)
-    points_right = np.zeros_like(points_left)
+    mask = cv2.inRange(hsv, lower_white, upper_white)
 
-    for i, match in enumerate(matches):
-        points_left[i, :] = keypoints_left[match.queryIdx].pt
-        points_right[i, :] = keypoints_right[match.trainIdx].pt
+    h, w = mask.shape[:2]
 
-    return points_left, points_right
+    # Clear top fraction of the image
+    f = 0.2
+    mask[0:int(np.ceil(f * h)), 0:w] = 0
 
-def stitch_images(left_img, right_img, points_left, points_right):
-    # Find the homography matrix
-    H, _ = cv2.findHomography(points_right, points_left, method=cv2.RANSAC)
+    # Set mask on robot body
+    xs, xe = int(np.ceil(0.3 * w)), int(np.ceil(0.7 * w))
+    ys = int(np.ceil(0.5 * h))
+    mask[ys:h, xs:xe] = 0
 
-    # Warp the right image to align with the left image
-    height, width, _ = left_img.shape
-    warped_img = cv2.warpPerspective(right_img, H, (width, height))
+    # Hough lines to remove spray
+    mask_lanes = mask.copy()
+    mask_lanes[0:h, 0:w] = 0
 
-    # Stitch the images together
-    result = np.zeros((height, 2 * width, 3), dtype=np.uint8)
-    result[0:height, 0:width] = left_img
-    result[0:height, width:2*width] = warped_img
+    hough = cv2.HoughLinesP(image=mask, rho=1, theta=10 * (np.pi) / 180, threshold=50, minLineLength=50, maxLineGap=100)
+    if hough is not None:
+        for points in hough:
+            x0, y0, x1, y1 = points[0]
+            cv2.line(mask_lanes, (x0, y0), (x1, y1), color=(255, 255, 255), thickness=2)
 
-    return result
+    stacked_img = np.stack((mask_lanes,) * 3, axis=-1)
+    return stacked_img
+
+def stitch_images(left_img, right_img):
+    # Use OpenCV's stitcher to combine images
+    stitcher = cv2.Stitcher.create(cv2.Stitcher_PANORAMA)
+    (status, stitched) = stitcher.stitch([left_img, right_img])
+    
+    if status == cv2.Stitcher_OK:
+        return stitched
+    else:
+        rospy.loginfo("Image stitching failed.")
+        return None
+
+def publish_panoramic():
+    global left_image, right_image
+
+    if left_image is not None and right_image is not None:
+        left_processed = process_image(left_image)
+        right_processed = process_image(right_image)
+
+        panoramic_img = stitch_images(left_processed, right_processed)
+        if panoramic_img is not None:
+            pub.publish(br.cv2_to_imgmsg(panoramic_img, "bgr8"))
 
 if __name__ == "__main__":
-    image_path = '/Users/hannabulinda/Desktop/Stereo_Image.png'  
+    rospy.init_node("hsv_thresh", anonymous=False)
+    pub = rospy.Publisher("/panoramic_lane_img", Image, queue_size=10)
+    cfg = Server(HSVConfig, callback)
+    rospy.Subscriber("/zed2/zed_node/left/image_rect_color", Image, left_img_cb)
+    rospy.Subscriber("/zed2/zed_node/right/image_rect_color", Image, right_img_cb)
 
-    left_img, right_img = load_and_split_stereo_image(image_path)
-    points_left, points_right = detect_and_match_features(left_img, right_img)
-    panoramic_img = stitch_images(left_img, right_img, points_left, points_right)
-
-    cv2.imshow('Panoramic Image', panoramic_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    rate = rospy.Rate(10)  # Adjust the rate as needed
+    while not rospy.is_shutdown():
+        publish_panoramic()
+        rate.sleep()
